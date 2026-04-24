ecdbde868b2f834b12da4131b33ff6eeee653819
Adeniran israel <adenisrael97@gmail.com>
2026-04-24 16:00:02 +0100

    Add landing page

---
+++ frontend/src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>TRANSHUB 🚚</h1>
+      <p>Your logistics platform is live</p>
+    </div>
+  )
+}
